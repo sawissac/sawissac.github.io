c365e8959f8409e2ed39f169a92b4bffc18d2bb0
Saw Issac <issacmm64@gmail.com>
2022-05-05 16:30:46 +0630

    startup

---
+++ js/cursor.js
@@ -0,0 +1,30 @@
+const curDot = window.curDot;
+
+const cursor = curDot({
+  diameter: 30,
+  borderWidth: 1,
+  borderColor: "#fff",
+  easing: 4,
+  background: "transparent",
+  zIndex: 9999,
+});
+
+cursor.over("#cur-dot-moti", {
+  scale: 3,
+  background: "white",
+});
+
+cursor.over(".next-link", {
+  scale: 3,
+  background: "white",
+});
+
+cursor.over(".user-portrait", {
+  scale: 3,
+  background: "red",
+});
+
+cursor.over("#next-link-action", {
+  scale: 3,
+  background: "white",
+});
